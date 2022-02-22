d3fa312b46d3d2f68180205e62bbb1d231636c17
“Joe <joe_simmons_99@yahoo.com>
2022-02-22 13:19:10 -0800

    completed excercises

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # csv_excercises_with_python
+
+This excercise will do the following:
+1. Concatenate two files into one
+2. Convert a column datatype from an 'object' to 'datetime64'
+3. Create various pivot tables
+4. Save pivoted data to summary files
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import re
 import pandas as pd
 import numpy as np
 import glob
+import os
 
-# import all files as one dataframe
-files = glob.glob('*.csv')
+# import all files from the "files" folder as one dataframe
+files = glob.glob("files/*.csv")
 
 # print(files) 
 
@@ -29,6 +31,22 @@ print(sales.dtypes) # confirm the data type of the 'date' column has changed
 # # print the 'order' column
 # print(sales['order'])
 
-# remove the '#' character from the 'order' column
+# # remove the '#' character from the 'order' column
 sales['order'] = sales['order'].str.replace('#', '')
 # print(sales['order']) # confirm the '#' character has been removed
+
+# create a pivot table to show the total sales for each product
+total_sales = pd.pivot_table(sales, index='item', values='qty', aggfunc=np.sum)
+sales_by_vendor = pd.pivot_table(sales, index=['vendor'], values='qty', aggfunc=np.sum)
+sales_by_vendor_and_item = pd.pivot_table(sales, index=['vendor', 'item'], values='qty', aggfunc=np.sum)
+
+# print the pivot table and order by the total sales in descending order
+print(total_sales.sort_values(by='qty', ascending=False))
+print(sales_by_vendor.sort_values(by='qty', ascending=False))
+
+# write the pivot table to a csv file and order by total sales in descending order and place it in the 'files' folder
+# sort the pivot table by total sales in descending order
+# if the files below exist, delete them first before writing the new pivot table
+total_sales.sort_values(by='qty', ascending=False).to_csv('files/total_sales.csv')
+sales_by_vendor.sort_values(by='qty', ascending=False).to_csv('files/sales_by_vendor.csv')
+sales_by_vendor_and_item.sort_values(by='qty', ascending=False).to_csv('files/sales_by_vendor_and_item.csv')
